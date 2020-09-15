eeb7a077807b06a71f80df615bd991be4d4e85fb
Dwozn <dominika.wozniczka@racing.agh.edu.pl>
2020-09-15 18:12:42 +0200

    callback configuration

--- page_buttons/page_buttons.ino
+++ page_buttons/page_buttons.ino
@@ -1,53 +1,53 @@
-#include <WiFi.h> 
+#include <WebServer.h> 
 
 const char* ssid     = "abcde";
 const char* password = "12345";
 
-WiFiClient client;
-WiFiServer server(80);
-
-unsigned long currentTime = millis();
-unsigned long previousTime = 0; 
-const long timeoutTime = 2000;
-
-int counter = 0;
-String header;
+WebServer server(80);
 
 void rootHandle(void){
-  client.println("<!DOCTYPE html><HTML>");
-  client.println("<BODY><h1> </h1>");
-  client.println("<a href=\"/current\"><button class=\"button\">Current</button> </a>");
-  client.println("<a href=\"/voltage\"><button class=\"button\">Voltage</button> </a>");
-  client.println("<a href=\"/config\"><button class=\"button\">Config</button> </a>");
-  client.println("</BODY>");
-  client.println("</HTML>"); 
+  String message = "<!DOCTYPE html><HTML>";
+  message +=       "<BODY><h1> </h1>";
+  message +=       "<a href=\"/current\"><button class=\"button\">Current</button> </a>";
+  message +=       "<a href=\"/voltage\"><button class=\"button\">Voltage</button> </a>";
+  message +=       "<a href=\"/config\"><button class=\"button\">Config</button> </a>";
+  message +=       "</BODY>";
+  message +=       "</HTML>";
+
+  server.send(200,"text/html", message);
 }
 
 void configHandle(void){
-  client.println("<!DOCTYPE html><HTML>");
-  client.println("<BODY><h1>Config</h1>");
-  client.println("<a href=\"/current\"><button class=\"button\">Current</button> </a>");
-  client.println("<a href=\"/voltage\"><button class=\"button\">Voltage</button> </a>");
-  client.println("</BODY>");
-  client.println("</HTML>"); 
+  String message = "<!DOCTYPE html><HTML>";
+  message +=       "<BODY><h1>Config</h1>";
+  message +=       "<a href=\"/current\"><button class=\"button\">Current</button> </a>";
+  message +=       "<a href=\"/voltage\"><button class=\"button\">Voltage</button> </a>";
+  message +=       "</BODY>";
+  message +=       "</HTML>"; 
+
+  server.send(200,"text/html", message);
 }
 
 void currentHandle(void){
-  client.println("<!DOCTYPE html><HTML>");
-  client.println("<BODY><h1>Current</h1>");
-  client.println("<a href=\"/voltage\"><button class=\"button\">Voltage</button> </a>");
-  client.println("<a href=\"/config\"><button class=\"button\">Config</button> </a>");
-  client.println("</BODY>");
-  client.println("</HTML>"); 
+  String message = "<!DOCTYPE html><HTML>";
+  message +=       "<BODY><h1>Current</h1>";
+  message +=       "<a href=\"/config\"><button class=\"button\">Config</button> </a>";
+  message +=       "<a href=\"/voltage\"><button class=\"button\">Voltage</button> </a>";
+  message +=       "</BODY>";
+  message +=       "</HTML>"; 
+
+  server.send(200,"text/html", message);
 }
 
 void voltageHandle(void){
-  client.println("<!DOCTYPE html><HTML>");
-  client.println("<BODY><h1>Voltage</h1>");
-  client.println("<a href=\"/current\"><button class=\"button\">Current</button> </a>");
-  client.println("<a href=\"/config\"><button class=\"button\">Config</button> </a>");
-  client.println("</BODY>");
-  client.println("</HTML>"); 
+  String message = "<!DOCTYPE html><HTML>";
+  message +=       "<BODY><h1>Voltage</h1>";
+  message +=       "<a href=\"/current\"><button class=\"button\">Current</button> </a>";
+  message +=       "<a href=\"/config\"><button class=\"button\">Config</button> </a>";
+  message +=       "</BODY>";
+  message +=       "</HTML>"; 
+
+  server.send(200,"text/html", message);
 }
 
 void setup(void){   
@@ -65,56 +65,14 @@ void setup(void){
   Serial.print("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP()); 
-  server.begin();
+  
+  server.on("/voltage", voltageHandle);
+  server.on("/current", currentHandle);
+  server.on("/config", configHandle);
+  server.on("/", rootHandle);
+    server.begin();
 }
-void loop(){
-  client = server.available();  
 
-  if (client) {                           
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          
-    String currentLine = "";                
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  
-      currentTime = millis();
-      if (client.available()) {             
-        char c = client.read();             
-        Serial.write(c);                           
-        header += c;
-        if (c == '\n') {    
-          if (currentLine.length() == 0) {           
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println(); 
-                       
-            if (header.indexOf("GET /voltage") >= 0) {       
-              voltageHandle();            
-            }  
-            else if (header.indexOf("GET /current") >= 0) {       
-              currentHandle();            
-            }  
-            else if (header.indexOf("GET /config") >= 0) {       
-              configHandle();            
-            }   
-            else if (header.indexOf("GET /") >= 0) {       
-              rootHandle();            
-            }                     
-          } 
-          else { 
-            currentLine = "";
-          }
-        } 
-        else if (c != '\r') {  
-          currentLine += c;      
-        }
-      }
-    }    
-   
-    header = "";
-    
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+void loop(){
+  server.handleClient();  
 }
