c16ab4270970c3585959b464289802699e6fc745
Dwozn <dominika.wozniczka@racing.agh.edu.pl>
2020-09-15 17:57:04 +0200

    callback configuration

--- counter/counter.ino
+++ counter/counter.ino
@@ -1,25 +1,21 @@
-#include <WiFi.h> 
+#include <WebServer.h>
 
 const char* ssid     = "abcde";
 const char* password = "12345";
 
-WiFiClient client;
-WiFiServer server(80);
-
-unsigned long currentTime = millis();
-unsigned long previousTime = 0; 
-const long timeoutTime = 2000;
+WebServer server(80);
 
 int counter = 0;
-String header;
 
 void counterHandle(void){
-  client.println("<!DOCTYPE html><HTML>");
-  client.println("<BODY><h1>Counter</h1>");
-  client.println(counter);
-  client.println("</BODY>");
-  client.println("</HTML>"); 
+  String message = "<!DOCTYPE html><HTML>";
+  message +=       "<BODY><h1>Counter</h1>";
+  message +=        String(counter);
+  message +=        "</BODY>";
+  message +=        "</HTML>";
 
+  server.send(200,"text/html", message);
+  
   counter++;
 }
 
@@ -38,48 +34,12 @@ void setup(void){
   Serial.print("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP()); 
+
+  server.on("/counter", counterHandle);
+
   server.begin();
 }
 
 void loop(){
-  client = server.available();   
-
-  if (client) {                             
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          
-    String currentLine = "";                
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  
-      currentTime = millis();
-      if (client.available()) {             
-        char c = client.read();            
-        Serial.write(c);                         
-        header += c;
-        if (c == '\n') {  
-          if (currentLine.length() == 0) {           
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();           
-                     
-            if (header.indexOf("GET /counter") >= 0) {       
-              counterHandle();            
-            }            
-          }
-          else {
-            currentLine = "";
-          }
-        } 
-        else if (c != '\r') { 
-          currentLine += c;     
-        }
-      }
-    }    
-    
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+  server.handleClient();  
 }
