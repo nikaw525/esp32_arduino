94d9fdd6c42bbc2efee676d765628075eb016aa8
Dwozn <dwozniczka@student.agh.edu.pl>
2020-11-11 18:23:20 +0100

    p_n form with 3 sections

--- p_n_form/p_n_form.ino
+++ p_n_form/p_n_form.ino
@@ -6,11 +6,15 @@ const char* password = "1234567890";
 WebServer server(80);
 
 int Mcounter = 10;
+int Acounter = 10;
+int Dcounter = 10;
 
 void rootHandle(void){
   String message = "<!DOCTYPE html><HTML>";
   message +=       "<BODY><h1>Pick section</h1>";
   message +=       "<a href=\"/Mcounter\"><button class=\"button\">M section</button> </a>";
+  message +=       "<a href=\"/Acounter\"><button class=\"button\">A section</button> </a>";
+  message +=       "<a href=\"/Dcounter\"><button class=\"button\">D section</button> </a>";
   message +=        "</BODY>";
   message +=        "</HTML>";
 
@@ -27,7 +31,7 @@ void McounterHandle(void){
   }
   
   String message = "<!DOCTYPE html><HTML>";
-  message +=       "<BODY><h1>Plus / minus</h1>";
+  message +=       "<BODY><h1>Sekcja M: Plus / minus</h1>";
   message +=       "Value: ";
   message +=        String(Mcounter);
   message +=        "<br>";  
@@ -41,11 +45,61 @@ void McounterHandle(void){
   server.send(200,"text/html", message);  
 }
 
+void AcounterHandle(void){  
+
+  if(server.arg("p_n") == "p"){
+      Acounter = Acounter + 1;
+  }
+  if(server.arg("p_n") == "n"){
+      Acounter = Acounter - 1;
+  }
+  
+  String message = "<!DOCTYPE html><HTML>";
+  message +=       "<BODY><h1>Sekcja A: Plus / minus</h1>";
+  message +=       "Value: ";
+  message +=        String(Acounter);
+  message +=        "<br>";  
+  message +=        "<a href=\"/Acounter?p_n=p\"><button class=\"button\">p</button> </a>";
+  message +=        "<a href=\"/Acounter?p_n=n\"><button class=\"button\">n</button> </a>";
+  message +=        "<br>";
+  message +=       "<a href=\"/\"><button class=\"button\">Back to Pick section</button> </a>";
+  message +=        "</BODY>";
+  message +=        "</HTML>";  
+  
+  server.send(200,"text/html", message);  
+}
+
+void DcounterHandle(void){  
+
+  if(server.arg("p_n") == "p"){
+      Dcounter = Dcounter + 1;
+  }
+  if(server.arg("p_n") == "n"){
+      Dcounter = Dcounter - 1;
+  }
+  
+  String message = "<!DOCTYPE html><HTML>";
+  message +=       "<BODY><h1>Sekcja D: Plus / minus</h1>";
+  message +=       "Value: ";
+  message +=        String(Dcounter);
+  message +=        "<br>";  
+  message +=        "<a href=\"/Dcounter?p_n=p\"><button class=\"button\">p</button> </a>";
+  message +=        "<a href=\"/Dcounter?p_n=n\"><button class=\"button\">n</button> </a>";
+  message +=        "<br>";
+  message +=       "<a href=\"/\"><button class=\"button\">Back to Pick section</button> </a>";
+  message +=        "</BODY>";
+  message +=        "</HTML>";  
+  
+  server.send(200,"text/html", message);  
+}
+
 void setup(void){    
   WiFi.mode(WIFI_AP); //Access Point mode
   WiFi.softAP(ssid, password);    //Password length minimum 8 char 
   server.on("/", rootHandle);    
   server.on("/Mcounter", McounterHandle);    
+  server.on("/Dcounter", DcounterHandle);   
+  server.on("/Acounter", AcounterHandle);   
   server.begin();
  
 }
