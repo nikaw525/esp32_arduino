d3c088bbe52762aac2c8ceb5eea1ec288d2f674d
Dwozn <dwozniczka@student.agh.edu.pl>
2020-11-16 17:23:26 +0100

    Changed names of sections, sends numbers instead of letters for section.

--- p_n_form/p_n_form.ino
+++ p_n_form/p_n_form.ino
@@ -12,28 +12,12 @@ struct sekcje
   float CurrCounter;
 };
 
-sekcje Sekcje[3];
-
-String insert_string(String first, String second){
-
-  String output = first + second;
-
-  return output;
-} 
+sekcje Sekcje[5];
 
 void htmlHandle(void){
     
-  int sec_number;
-  
-  if(server.arg("section") == "M"){    
-    sec_number = 0;
-  }
-  else if(server.arg("section") == "D"){
-    sec_number = 1;
-  }
-  else if(server.arg("section") == "A"){
-    sec_number = 2;
-  }
+  int sec_number = server.arg("section").toInt();  
+ 
   if(server.arg("p_n") == "p"){
       Sekcje[sec_number].VoltCounter = Sekcje[sec_number].VoltCounter + 1;      
   }
@@ -45,24 +29,8 @@ void htmlHandle(void){
   
     String message = "<!DOCTYPE html><HTML>";
   message +=       "<BODY><h1>";
-  message +=       server.arg("section");  
+  message +=       Sekcje[sec_number].SectionName;  
   message +=       " Section</h1>";
-  message +=       "<HEAD>";
-  message +=       "<meta name='viewport' content='width=device-width, initial-scale=1'>";
-  message +=       "<STYLE>";
-  message +=       ".button{";
-  message +=       "display: block;";
-  message +=       "padding: 14px 40px;";
-  message +=       "width: 100%;";
-  message +=       "cursor: pointer;";
-  message +=       "border: none;";
-  message +=       "background-color: #555555;";
-  message +=       "font-size: 20px;";
-  message +=       "font-weight: bold;";
-  message +=       "color: #DFDC10;";
-  message +=       "}";
-  message +=       "</STYLE>";
-  message +=       "</HEAD>";
   message +=       "Voltage: ";
   message +=        String(Sekcje[sec_number].VoltCounter);
   message +=       " Current: ";
@@ -70,7 +38,7 @@ void htmlHandle(void){
   message +=        "<br>";
   message +=        "<form action='/Section'>";
   message +=        "<input type=hidden name=section value=";
-  message +=        Sekcje[sec_number].SectionName; 
+  message +=        String(sec_number); 
   message +=        "><button class=button name='p_n' type='submit' value='p'>p</button>";
   message +=        "<button class=button name='p_n' type='submit' value='n'>n</button>";
   message +=        "</form><br>";
@@ -87,9 +55,11 @@ void rootHandle(void){
   message +=       "<BODY><h1>Pick section</h1>";
   message +=       "<form action='/Section'>";
 
-  for(int i = 0; i<3; i++){        
-      message += insert_string("<button class=button name='section' type='submit' value=", Sekcje[i].SectionName);
-      message += insert_string(">", Sekcje[i].SectionName);
+  for(int i = 0; i<5; i++){        
+      message += "<button class=button name='section' type='submit' value=";
+      message +=  String(i);
+      message +=  ">";
+      message +=  Sekcje[i].SectionName;      
       message += " section</button> </a>";
   }
   message +=        "</form>";
@@ -101,15 +71,20 @@ void rootHandle(void){
 
 void setup(void){ 
 
-  Sekcje[0].SectionName = "M";
+  Sekcje[0].SectionName = "Vddm";
   Sekcje[0].VoltCounter = 10;
   
-  Sekcje[1].SectionName = "D";
+  Sekcje[1].SectionName = "Vdda";
   Sekcje[1].VoltCounter = 10;
   
-  Sekcje[2].SectionName = "A";
+  Sekcje[2].SectionName = "Vdiscr";
   Sekcje[2].VoltCounter = 10;
-  
+
+  Sekcje[3].SectionName = "Vcore";
+  Sekcje[3].VoltCounter = 10;
+
+  Sekcje[4].SectionName = "Vlvds ";
+  Sekcje[4].VoltCounter = 10;
   
   WiFi.mode(WIFI_AP); //Access Point mode
   WiFi.softAP(ssid, password);    //Password length minimum 8 char 
