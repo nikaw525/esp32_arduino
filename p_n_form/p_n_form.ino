7cc8e90e79b94bbb5f9f9b68ce4cfcff438af535
Dwozn <dwozniczka@student.agh.edu.pl>
2020-11-18 16:18:38 +0100

    added css file with page style

--- p_n_form/p_n_form.ino
+++ p_n_form/p_n_form.ino
@@ -1,5 +1,6 @@
 #include <WebServer.h>
 
+#include "stylesheet.h"
 const char* ssid     = "Esp-Access-Point";
 const char* password = "1234567890";
 
@@ -35,22 +36,26 @@ void htmlHandle(void){
 
   Sekcje[sec_number].CurrCounter = Sekcje[sec_number].VoltCounter*1.5;
   
-    String message = "<!DOCTYPE html><HTML>";
-  message +=       "<BODY><h1>";
+    String message = "<!DOCTYPE html><HTML>";  
+  message +=       "<HEAD>";
+  message +=       "<link rel='stylesheet' type='text/css' href='stylesheet.css' media='screen'/>";
+  message +=       "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+  message +=       "</HEAD>";
+  message +=       "<BODY><h2><span>";
   message +=       Sekcje[sec_number].SectionName;  
-  message +=       " Section</h1>";
-  message +=       "Voltage: ";
+  message +=       " Section<span></h2>";
+  message +=       "<p1>Voltage: ";
   message +=        String(Sekcje[sec_number].VoltCounter);
-  message +=       " Current: ";
+  message +=       "</p1><p1>Current: ";
   message +=        String(Sekcje[sec_number].CurrCounter);
-  message +=        "<br>";
+  message +=        "</p1><br><br><br>";
   message +=        "<form action='/Section'>";
   message +=        "<input type=hidden name=section value=";
   message +=        String(sec_number); 
-  message +=        "><button class=button name='p_n' type='submit' value='p'>p</button>";
-  message +=        "<button class=button name='p_n' type='submit' value='n'>n</button>";
+  message +=        "><button class=button_1 name='p_n' type='submit' value='p'>p</button><br>";
+  message +=        "<button class=button_1 name='p_n' type='submit' value='n'>n</button><br>";
   message +=        "</form><br>";
-  message +=       "<a href=\"/\"><button class=\"button\">Back to Pick section</button> </a>";
+  message +=       "<a href=\'/\'><button class=button_2>Back to<br>Pick section</button> </a>";
   message +=        "</BODY>";
   message +=        "</HTML>";  
   
@@ -60,11 +65,15 @@ void htmlHandle(void){
 void rootHandle(void){  
 
   String message = "<!DOCTYPE html><HTML>";
-  message +=       "<BODY><h1>Pick section</h1>";
+  message +=       "<HEAD>";
+  message +=       "<link rel='stylesheet' type='text/css' href='stylesheet.css' media='screen'/>";
+  message +=       "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+  message +=       "</HEAD>";
+  message +=       "<BODY><h2><span>Pick section</span></h2>";
   message +=       "<form action='/Section'>";
 
   for(int i = 0; i<number_of_sect; i++){        
-      message += "<button class=button name='section' type='submit' value=";
+      message += "<button class=button_1 name='section' type='submit' value=";
       message +=  String(i);
       message +=  ">";
       message +=  Sekcje[i].SectionName;      
@@ -76,15 +85,19 @@ void rootHandle(void){
 
   server.send(200,"text/html", message);
 }
-
-void setup(void){ 
-
+void cssHandle(void){
   
+  String message = CSS_page;
+
+  server.send(200,"text/css", message);
+}
+void setup(void){ 
     
   WiFi.mode(WIFI_AP); //Access Point mode
   WiFi.softAP(ssid, password);    //Password length minimum 8 char 
   server.on("/", rootHandle);    
-  server.on("/Section", htmlHandle);        
+  server.on("/Section", htmlHandle);  
+  server.on("/stylesheet.css", cssHandle);  
   server.begin(); 
 }
 
---
+++ p_n_form/stylesheet.h
@@ -0,0 +1,54 @@
+const char CSS_page[] PROGMEM = R"=====(
+.button_1{	  
+  display: block;
+  height: 100px;
+  width: 100%;	
+  border-radius: 8px;  
+  background-color: #262626;	  
+  font-weight: bold;
+  font-size: 50px;	  
+  text-align: center;
+  color: #DFDC10; 	  
+}
+.button_2{
+  display: block;
+  height: 200px;
+  width: 100%;	 
+  border-radius: 8px;
+  background-color: #595959;	  
+  font-weight: bold;
+  font-size: 50px;	  
+  text-align: center;
+  color: #DFDC10;    
+}
+p1 {		
+  display: in-line;  
+  height: 100px;  
+  width: 60px;
+  border: none;
+  border-radius: 8px; 	  
+  background-color: #262626;
+  padding: 15px 15px 15px 15px;
+  font-weight: bold;
+  font-size: 20px;	  
+  text-align: center;
+  color: #DFDC10; 
+}
+body{
+	background-color:#d9d9d9;
+}
+h2 {
+   width: 100%; 
+   text-align: center; 
+   border-bottom: 2px solid #262626; 
+   line-height: 0.1em;   
+   margin: 40px 0 40px; 
+   text-align: center;
+   font-size: 40px;
+   color: #262626;
+} 
+h2 span { 
+	background:#d9d9d9; 
+	padding:0px 5px; 
+}
+)=====";
